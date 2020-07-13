6afeae607fe2113d492f1e5eb7936c5bed6d1e36
Sam Snarr <sssnarr1@gmail.com>
2020-07-13 13:06:20 -0400

    added mongoDB schema and configuration with MongoDB cloud

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,9 +9,15 @@ const {
 
 const router = express.Router();
 
-// prettier ignore
-router.route("/").get(getBootcamps).post(createBootcamp);
+router
+    .route("/")
+    .get(getBootcamps)
+    .post(createBootcamp);
 
-router.route("/:id").get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp);
+router
+    .route("/:id")
+    .get(getBootcamp)
+    .put(updateBootcamp)
+    .delete(deleteBootcamp);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+
+const logger = require("./middleware/logger");
+const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db");
 
 const app = express();
-const bootcamps = require("./routes/bootcamps");
 
 // load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// connect to database
+connectDB();
+
+// use morgan logging if in development mode
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
 
 // mount routers
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}` .bold.yellow));
+
+// handle a failed connection to the database
+process.on("unhandledRejection", (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    server.close(() => process.exit(1));
+});
